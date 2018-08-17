3e3f0ec02745f4f0f9c199443b7318485564d9bf
cruizba <charlesrb94@outlook.com>
2018-08-18 01:36:18 +0200

    docker-compose works in all ubuntu distributions

--- class-recorder-teacher-pc-server/src/main/java/com/classrecorder/teacherserver/modules/ffmpegwrapper/ICommandLinux.java
+++ class-recorder-teacher-pc-server/src/main/java/com/classrecorder/teacherserver/modules/ffmpegwrapper/ICommandLinux.java
@@ -62,7 +62,7 @@ class ICommandLinux implements ICommand{
 		List<String> command = new ArrayList<>();
         command.addAll(Arrays.asList("ffmpeg", "-f", "x11grab", "-r", Integer.toString(frameRate)));
         command.addAll(Arrays.asList("-s", screenWidth + "x" + screenHeight, "-i", x11device));
-        command.addAll(Arrays.asList("-vcodec", "h264"));
+        command.addAll(Arrays.asList("-vcodec", "h264", "-thread_queue_size", "20480"));
         command.addAll(Arrays.asList("-preset", "ultrafast", "-crf", "30"));
         command.addAll(Arrays.asList(directory + "/" + name + "." + cFormat));
 		logCommand(command);
