1c360e055a65e6ff89f7dc27c888bfe6d7fb096a
cruizba <charlesrb94@outlook.com>
2018-08-22 17:06:12 +0200

    Improved docker compose

--- docker_prod/class-recorder-teacher-pc/run.sh
+++ docker_prod/class-recorder-teacher-pc/run.sh
@@ -3,7 +3,4 @@ xhost +local:
 echo "Opened xhost to local connections"
 export AUDIO_GROUP_CR=$(getent group audio | cut -d: -f3)
 echo "Created AUDIO_GROUP_CR environment variable to ${AUDIO_GROUP_CR}"
-echo ${MYSQL_URL_CR}
-echo ${YOUTUBE_CLIENT_ID}
-echo ${YOUTUBE_SECRET}
 docker-compose up
