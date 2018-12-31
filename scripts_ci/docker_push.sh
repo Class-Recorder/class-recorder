5cb311df5e4c3f2e048872a982858d01d0343c19
cruizba <charlesrb94@outlook.com>
2018-12-31 18:23:39 +0000

    Automatic tags for docker production images

--- scripts_ci/docker_push.sh
+++ scripts_ci/docker_push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd docker-class-recorder/docker-runnables/crecorder-pc-prod
 # Docker login and push of crecorder-pc-prod
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . -t cruizba/crecorder-pc-prod:$TRAVIS_TAG
--- scripts_ci/travis.sh
+++ scripts_ci/travis.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-pwd
-mkdir class-recorder
 git clone https://github.com/Class-Recorder/docker-class-recorder
 cd docker-class-recorder/docker-runnables/crecorder-ci
 ./docker_run.sh
