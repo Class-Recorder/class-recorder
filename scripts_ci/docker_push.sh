ac50cc4ad280984912dbb48dfd61bd3e975f4604
Carlos Ruiz Ballesteros <charlesrb94@outlook.com>
2018-12-31 20:58:04 +0100

    Update docker_push.sh

--- scripts_ci/docker_push.sh
+++ scripts_ci/docker_push.sh
@@ -3,6 +3,7 @@
 ls -l
 pwd
 cd docker-class-recorder/docker-runnables/crecorder-pc-prod
+ls -l
 # Docker login and push of crecorder-pc-prod
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build . -t cruizba/crecorder-pc-prod:$TRAVIS_TAG
